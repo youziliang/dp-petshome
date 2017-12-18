0e7a645c79f11ff3966e0a3eb4043ce82f2770b2
youziliang <345717321@qq.com>
2017-12-18 18:27:50 +0800

    databases

--- dp-petshome-service/src/main/java/com/dp/petshome/service/impl/DubboDemoServiceImpl.java
+++ dp-petshome-service/src/main/java/com/dp/petshome/service/impl/DubboDemoServiceImpl.java
@@ -1,5 +1,7 @@
 package com.dp.petshome.service.impl;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
 
 import com.dp.petshome.service.DubboDemoService;
@@ -7,8 +9,11 @@ import com.dp.petshome.service.DubboDemoService;
 @Service
 public class DubboDemoServiceImpl implements DubboDemoService {
 
+	private static final  Logger log  =  LoggerFactory.getLogger(DubboDemoServiceImpl.class );
+	
 	@Override
 	public String sayHello(String name) {
+		log.info("log --> Provider的Info日志测试");
 	        return "Hello " + name;
 	}
 }
