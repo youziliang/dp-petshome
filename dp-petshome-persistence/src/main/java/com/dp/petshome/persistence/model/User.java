4207009c1b305aad2880420a7cb02d376940f22d
youziliang <345717321@qq.com>
2017-12-18 21:24:14 +0800

    wwwwww

--- dp-petshome-persistence/src/main/java/com/dp/petshome/persistence/model/User.java
+++ dp-petshome-persistence/src/main/java/com/dp/petshome/persistence/model/User.java
@@ -1,8 +1,7 @@
 package com.dp.petshome.persistence.model;
 
-import java.io.Serializable;
-
-public class User implements Serializable {
+public class User {
+	
     private Integer id;
 
     private String name;
